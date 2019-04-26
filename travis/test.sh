b4185fc98e4a0c61fe73695236a409f495c45fad
Andrei Vagin <avagin@google.com>
2019-04-26 10:41:45 -0700

    travis

--- travis/test.sh
+++ travis/test.sh
@@ -2,6 +2,8 @@
 
 set -x -e
 
+BAZEL_OPTS="--show_progress_rate_limit=5 --show_task_finish --show_timestamps"
+
 if [ "$TEST_SUITE" == "make" ]; then
   make BAZEL_OPTIONS="build ..." bazel
   eval `make bazel-alias | sed 's/alias //'`
@@ -14,26 +16,26 @@ fi
 sudo ./travis/install-bazel.sh
 
 if [ "$TEST_SUITE" == "unit" ]; then
-  bazel test pkg/...
+  bazel test $BAZEL_OPTS `bazel query pkg/... | grep -v kvm_test`
   exit 0
 
 elif [ "$TEST_SUITE" == "docker" ]; then
-  bazel build runsc/tools/dockercfg/... runsc:runsc
+  bazel build $BAZEL_OPTS runsc/tools/dockercfg/... runsc:runsc
   ./runsc/test/install.sh --runtime runsc
   docker run --runtime=runsc hello-world
-  bazel test --test_env=RUNSC_RUNTIME=runsc \
+  bazel test $BAZEL_OPTS --test_env=RUNSC_RUNTIME=runsc \
     //runsc/test/image:image_test \
     //runsc/test/integration:integration_test
   exit 0
 
 elif [ "$TEST_SUITE" == "syscalls-aq" ]; then
-  tests=$(bazel query test/syscalls/... | grep -e 'syscalls:[a-q].*ptrace$')
-  bazel test $tests
+  tests=$(bazel query $BAZEL_OPTS test/syscalls/... | grep -e 'syscalls:[a-q].*ptrace$')
+  bazel test $BAZEL_OPTS $tests
   exit 0
 
 elif [ "$TEST_SUITE" == "syscalls-rz" ]; then
-  tests=$(bazel query test/syscalls/... | grep -e 'syscalls:[^a-q].*ptrace$')
-  bazel test $tests
+  tests=$(bazel query $BAZEL_OPTS test/syscalls/... | grep -e 'syscalls:[^a-q].*ptrace$')
+  bazel test $BAZEL_OPTS $tests
   exit 0
 
 else
